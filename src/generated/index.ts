81493e06baecad92e3adba33547e33ec02ba3e02
hinphansa <hin.phansa@gmail.com>
2023-10-24 18:08:09 +0700

    add constants and export generated types

--- src/generated/index.ts
+++ src/generated/index.ts
@@ -1,10 +1,44 @@
-export { IAPHCore__factory } from "./factories/IAPHCore__factory";
-export { IAPHLibrary__factory } from "./factories/IAPHLibrary__factory";
-export { IAPHPool__factory } from "./factories/IAPHPool__factory";
-export { IHelperCore__factory } from "./factories/IHelperCore__factory";
-export { IHelperFutureTrade__factory } from "./factories/IHelperFutureTrade__factory";
-export { IHelperMembershipAndStakePool__factory } from "./factories/IHelperMembershipAndStakePool__factory";
-export { IHelperPool__factory } from "./factories/IHelperPool__factory";
-export { IInterestVault__factory } from "./factories/IInterestVault__factory";
-export { IMembership__factory } from "./factories/IMembership__factory";
-export { IStakePool__factory } from "./factories/IStakePool__factory";
+// FACTORIES
+export { IAPHCore__factory } from './factories/IAPHCore__factory';
+export { IAPHLibrary__factory } from './factories/IAPHLibrary__factory';
+export { IAPHPool__factory } from './factories/IAPHPool__factory';
+export { IHelperCore__factory } from './factories/IHelperCore__factory';
+export { IHelperFutureTrade__factory } from './factories/IHelperFutureTrade__factory';
+export { IHelperMembershipAndStakePool__factory } from './factories/IHelperMembershipAndStakePool__factory';
+export { IHelperPool__factory } from './factories/IHelperPool__factory';
+export { IInterestVault__factory } from './factories/IInterestVault__factory';
+export { IMembership__factory } from './factories/IMembership__factory';
+export { IStakePool__factory } from './factories/IStakePool__factory';
+
+// INSTANCES
+export { APHLibrary, CoreBase, IAPHCore, IAPHCoreInterface } from './IAPHCore';
+export { IAPHLibrary, IAPHLibraryInterface } from './IAPHLibrary';
+export { IAPHPool, IAPHPoolInterface, PoolBase, PoolToken } from './IAPHPool';
+export { HelperBase, IHelperCore, IHelperCoreInterface } from './IHelperCore';
+export { IHelperFutureTrade, IHelperFutureTradeInterface } from './IHelperFutureTrade';
+export {
+  IHelperMembershipAndStakePool,
+  IHelperMembershipAndStakePoolInterface
+} from './IHelperMembershipAndStakePool';
+export { IHelperPool, IHelperPoolInterface } from './IHelperPool';
+export { IInterestVault, IInterestVaultInterface } from './IInterestVault';
+export {
+  ApprovalEvent,
+  ApprovalEventFilter,
+  ApprovalForAllEvent,
+  ApprovalForAllEventFilter,
+  IMembership,
+  IMembershipInterface,
+  TransferEvent,
+  TransferEventFilter
+} from './IMembership';
+export { IStakePool, IStakePoolInterface, StakePoolBase } from './IStakePool';
+export {
+  GetARGsTypeFromFactory,
+  GetContractTypeFromFactory,
+  MinEthersFactory,
+  OnEvent,
+  TypedEvent,
+  TypedEventFilter,
+  TypedListener
+} from './common';
